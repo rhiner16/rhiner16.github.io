fb24b7402f229fcec04b8dc7286bf94c9d8fdfdd
rhiner16 <97468234+rhiner16@users.noreply.github.com>
2025-02-13 21:05:43 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -30,4 +30,22 @@ function plusSlides(n, slideshowId) {
     showSlides(slideIndexes[slideshowId] += n, slideshowId);
 }
 
-
+document.addEventListener('DOMContentLoaded', () => {
+    const faders = document.querySelectorAll('.fade-in');
+    const appearOptions = {
+      threshold: 0.3,
+      rootMargin: "0px 0px -50px 0px"
+    };
+  
+    const appearOnScroll = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) return;
+        entry.target.classList.add('appear');
+        observer.unobserve(entry.target);
+      });
+    }, appearOptions);
+  
+    faders.forEach(fader => {
+      appearOnScroll.observe(fader);
+    });
+  });
